a49ffca29be5df3fa1dc84dee6512242cb2116a5
deadevilx <svserg88@gmail.com>
2022-11-15 16:29:06 +0200

    Add hometask 08 2.0

--- Tutorial_08/App/App.cpp
+++ Tutorial_08/App/App.cpp
@@ -105,52 +105,80 @@ int main(int argc, char* argv[]) {
 			end->next->next = NULL;
 			end = end->next;
 
-
+			count++;
 		}
 		else if (cmd[0] == 'i') {
 			int index, k = 0;
 			int new_value;
-			MyNode* temp, * p_node;
+			MyNode* new_node, * next_node, *end = &list;
 			sscanf(cmd, "%c %d %d", &cmd[0], &index, &new_value);
 
-			MyNode* end = &list;
-
-			if (index < 0 || index > count) {
-				printf("WRONG INDEX! Enter index from 0 to %d\n", count);
+			if (index <= 0 || index > count+1) {
+				printf("WRONG INDEX! Enter index from 1 to %d\n", count+1);
 			}
 			else {
-				while ((end->next != NULL) && (k != index)) {
+				while ((end->next != NULL) && (k != index - 1)) {
 					end = end->next;
 					k++;
 				}
-
-				temp = new MyNode;
-				p_node = end->next;   // указатель на след ноду
-				end->next = temp;     // предыдущий узел указывает на новый узел
-				temp->data = new_value;  // заполняем новый узел
-				temp->next = p_node;
-				temp->prev = end;
-				if (p_node != NULL) {
-					p_node->prev = temp; //след нода указивает на новый узел
+				new_node = new MyNode;
+				next_node = end->next;   // указатель на след ноду
+				end->next = new_node;     // текущая нода получает *next новой ноды
+				new_node->data = new_value;  // заполняем новую ноду
+				new_node->next = next_node;  //
+				new_node->prev = end;        //
+				if (next_node != NULL) {
+					next_node->prev = new_node; //след нода(если она не NULL) указывает на новую ноду
 				}
 				count++;
 			}
 		}
+		else if (cmd[0] == 'd') {
+			if (count != 0) {
+				int index, k = 0;
+				MyNode* next_node, * prev_node, * end = &list;
+				sscanf(cmd, "%c %d", &cmd[0], &index);
+
+				if (index <= 0 || index > count) {
+					printf("WRONG INDEX! Enter index from 1 to %d\n", count);
+				}
+				else {
+					while ((end->next != NULL) && (k != index)) {
+						end = end->next;
+						k++;
+					}
+					// указатель на удаляемой ноде
+					prev_node = end->prev;   // указатель на пред ноду
+					next_node = end->next;   // указатель на след ноду
+					if (prev_node != NULL) {
+						prev_node->next = end->next;  // нода перед удаляемой(если она не NULL) получает *next удаляемого
+					}
+					if (next_node != NULL) {
+						next_node->prev = end->prev;  //нода после удаляемой(если она не NULL) получает *prev удаляемого
+					}
+					delete end;               // удаляем ноду
+					count--;
+				}
+			}
+			else { printf("\nList is empty!\n"); }
+		}
+		
 				
 		  else if (cmd[0] == 'c') {
 
-			MyNode* end = &list;
+			MyNode* end = &list, * prev_node;
 
 			while (end->next != NULL) {
 				end = end->next;
 			}
-
+			//указатель находится на последней ноде
 			while (end->prev != NULL) {
-				MyNode* prev = end->prev;
-				end->prev->next = NULL;
-				delete end;
-				end = prev;
+				prev_node = end->prev;    // указатель на пред ноду
+				prev_node->next = NULL;   // пред нода получает в *next значение NULL
+				delete end;               // удаляем текущую ноду
+				end = prev_node;          // указатель становится на пред ноду
 			}
+			count = 0;
 		}
 		  else if (cmd[0] == 'e' || cmd[0] == 'q') {
 			break;
