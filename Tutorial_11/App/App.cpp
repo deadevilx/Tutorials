66df4cd20c57b8c8fc861532c3a198d5539ef7b5
deadevilx <svserg88@gmail.com>
2023-01-10 19:40:13 +0200

    Add hometask 11 v.1.0

--- Tutorial_11/App/App.cpp
+++ Tutorial_11/App/App.cpp
@@ -24,46 +24,76 @@ struct ThreadArgument {
 
 int value;
 std::mutex mtx;
+int wm;
+ThreadArgument arg_a = { &value, 1000 };
+ThreadArgument arg_b = arg_a;
+ThreadArgument arg_c = arg_a;
+ThreadArgument arg_d = arg_a;
 
 void inc_counter(ThreadArgument *arg) {
 	for (; arg->cycle_count > 0; arg->cycle_count--) {
+		if (wm == 1) {
+			mtx.lock();
+		}
 		int tmpval = *(arg->value);
 		std::this_thread::sleep_for(1ms);
 		tmpval += 1;
 		*(arg->value) = tmpval;
+		if (wm == 1) {
+			mtx.unlock();
+		}
 	}
 }
 
 void dec_counter(ThreadArgument *arg) {
 	for (; arg->cycle_count > 0; arg->cycle_count--) {
+		if (wm == 1) {
+			mtx.lock();
+		}
 		int tmpval = *(arg->value);
 		std::this_thread::sleep_for(1ms);
 		tmpval -= 1;
 		*(arg->value) = tmpval;
+		if (wm == 1) {
+			mtx.unlock();
+		}
+	}
+}
+
+void timer_func() {
+	unsigned secs = 0;
+	while (true) {
+		std::this_thread::sleep_for(1s);
+		secs++;
+		printf("sec = %d | value = %d | arg_a.count = %d | arg_b.count = %d | arg_c.count = %d | arg_d.count = %d\n", secs, value, arg_a.cycle_count, arg_b.cycle_count, arg_c.cycle_count, arg_d.cycle_count);
+		if (arg_a.cycle_count == 0 && arg_b.cycle_count == 0 && arg_c.cycle_count == 0 && arg_d.cycle_count == 0) {
+			printf("Value = %d\n", value);
+			break;
+		}
 	}
 }
 
 int main(int argc, char* argv[]) {
+	printf("Run program with Mutex or without? (0 - without|1 - with): ");
+	scanf("%d", &wm);
+
 	thread a;
 	thread b;
 	thread c;
 	thread d;
-
-	ThreadArgument arg_a = {&value, 1000};
-	ThreadArgument arg_b = arg_a;
-	ThreadArgument arg_c = arg_a;
-	ThreadArgument arg_d = arg_a;
+	thread timer;
 
 	a = thread(inc_counter, &arg_a);
 	b = thread(dec_counter, &arg_b);
 	c = thread(inc_counter, &arg_c);
 	d = thread(dec_counter, &arg_d);
+	timer = thread(timer_func);
 
 	a.join();
 	b.join();
 	c.join();
 	d.join();
-
+	timer.join();
 	printf("Press any key to continue ...\n");
 	int key = _getch();
 
