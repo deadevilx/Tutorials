1375d18f8794a7f716209255aabf92ee42844212
sentischev <sentischev@orizon-navigation.com>
2023-01-10 11:25:33 +0200

    Tutorial_11: part #2

--- Tutorial_11/App/App.cpp
+++ Tutorial_11/App/App.cpp
@@ -8,11 +8,14 @@
 #include <string.h>
 #include <time.h>
 #include <thread>
+#include <chrono>
+#include <mutex>
 
 #include "main.h"
 #include "misc.h"
 
 using std::thread;
+using namespace std::chrono_literals;
 
 struct ThreadArgument {
 	int *value;
@@ -20,16 +23,23 @@ struct ThreadArgument {
 };
 
 int value;
+std::mutex mtx;
 
 void inc_counter(ThreadArgument *arg) {
-	for (int i = 0; i < arg->cycle_count; i++) {
-		*(arg->value) += 1;
+	for (; arg->cycle_count > 0; arg->cycle_count--) {
+		int tmpval = *(arg->value);
+		std::this_thread::sleep_for(1ms);
+		tmpval += 1;
+		*(arg->value) = tmpval;
 	}
 }
 
 void dec_counter(ThreadArgument *arg) {
-	for (int i = 0; i < arg->cycle_count; i++) {
-		*(arg->value) -= 1;
+	for (; arg->cycle_count > 0; arg->cycle_count--) {
+		int tmpval = *(arg->value);
+		std::this_thread::sleep_for(1ms);
+		tmpval -= 1;
+		*(arg->value) = tmpval;
 	}
 }
 
@@ -39,12 +49,15 @@ int main(int argc, char* argv[]) {
 	thread c;
 	thread d;
 
-	ThreadArgument arg = {&::value, 1000000};
+	ThreadArgument arg_a = {&value, 1000};
+	ThreadArgument arg_b = arg_a;
+	ThreadArgument arg_c = arg_a;
+	ThreadArgument arg_d = arg_a;
 
-	a = thread(inc_counter, &arg);
-	b = thread(dec_counter, &arg);
-	c = thread(inc_counter, &arg);
-	d = thread(dec_counter, &arg);
+	a = thread(inc_counter, &arg_a);
+	b = thread(dec_counter, &arg_b);
+	c = thread(inc_counter, &arg_c);
+	d = thread(dec_counter, &arg_d);
 
 	a.join();
 	b.join();
--- Tutorial_11/README.md
+++ Tutorial_11/README.md
@@ -2,4 +2,4 @@
 
     A thread of execution is a sequence of instructions that can be executed concurrently with other such sequences in multithreading environments
 
-Please read this [page](https://cplusplus.com/reference/thread/thread/)
+Please read [this page](https://cplusplus.com/reference/thread/thread/) and [this one](https://cplusplus.com/reference/mutex/mutex/) too.
